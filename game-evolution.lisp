20b6fafd8f17b1cff7adea56e1435a3154afde7a
Endymion Kasanardjo <zetafish@gmail.com>
2011-03-22 20:55:01 +0100

    evolution working

--- game-evolution.lisp
+++ game-evolution.lisp
@@ -16,6 +16,8 @@
   (apply #'random-plant *jungle*)
   (random-plant 0 0 *width* *height*))
 
+;;; Animals in the world
+
 (defstruct animal x y energy dir genes)
 
 (defparameter *animals*
@@ -51,11 +53,69 @@
                      0
                      (1+ (angle (cdr genes) xnu))))))
       (setf (animal-dir animal)
-            (mod (+ (animal-dir animal) (angle (animal-genes animal x))
-                    8))))))
+            (mod (+ (animal-dir animal) (angle (animal-genes animal) x))
+                 8)))))
 
 (defun eat (animal)
   (let ((pos (cons (animal-x animal) (animal-y animal))))
     (when (gethash pos *plants*)
       (incf (animal-energy animal) *plant-energy*)
       (remhash pos *plants*))))
+
+(defparameter *reproduction-energy* 200)
+
+(defun reproduce (animal)
+  (let ((e (animal-energy animal)))
+    (when (>= e *reproduction-energy*)
+      (setf (animal-energy animal) (ash e -1))
+      (let ((animal-nu (copy-structure animal))
+            (genes     (copy-list (animal-genes animal)))
+            (mutation  (random 8)))
+        (setf (nth mutation genes)
+              (max 1 (+ (nth mutation genes) (random 3) -1)))
+        (setf (animal-genes animal-nu) genes)
+        (push animal-nu *animals*)))))
+
+(defun update-world ()
+  (setf *animals* (remove-if (lambda (animal) (<= (animal-energy animal) 0))
+                             *animals*))
+  (mapc (lambda (animal)
+          (turn animal)
+          (move animal)
+          (eat animal)
+          (reproduce animal))
+        *animals*)
+  (add-plants))
+
+(defun draw-world ()
+  (loop for y
+     below *height*
+     do (progn (fresh-line)
+               (princ "|")
+               (loop for x
+                  below *width*
+                  do (princ (cond ((some (lambda (animal)
+                                           (and (= (animal-x animal) x)
+                                                (= (animal-y animal) y)))
+                                         *animals*)
+                                   #\M)
+                                  ((gethash (cons x y) *plants*) #\*)
+                                  (t #\space))))
+               (princ "|"))))
+
+(defun evolution ()
+  (draw-world)
+  (fresh-line)
+  (let ((str (read-line)))
+    (cond ((equal str "quit") ())
+          (t (let ((x (parse-integer str  :junk-allowed t)))
+               (if x
+                   ( loop for i
+                          below x
+                          do (update-world)
+                          if (zerop (mod i 1000))
+                          do (princ #\.))
+                   (update-world)))
+             (evolution)))))
+
+
